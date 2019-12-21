500bf9914364cb9fe20187497406fe4907032d2e
Anthony McPherson <aqm28202@gmail.com>
2019-12-21 16:07:38 -0500

    updating .js to include ES6

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,53 +1,26 @@
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
-var burger = require("../models/burger.js");
+const burger = require("../models/burger");
 
-router.get("/", function (req, res) {
-  burger.all(function (data) {
-    var hbsObject = {
-      burgers: data
-    };
-    res.render("index", hbsObject);
-  });
-});
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        const hbObj = { burgers: data }
+        res.render("index", hbObj)
+    })
+})
 
-router.post("/api/burgers", function (req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function (result) {
-    res.json({ id: result.insertId });
-  });
-});
+router.post("/api/burger", (req, res) => {
+    burger.insert(req.body.burger_name, (data) => {
+        res.status(204).end()
+    })
+})
 
-router.put("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function (result) {
-    if (result.changedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function (result) {
-    if (result.affectedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
+router.put("/api/burger", (req, res) => {
+    burger.devour(req.body.id, (data) => {
+        res.status(204).end()
+    })
+})
 
 module.exports = router;
